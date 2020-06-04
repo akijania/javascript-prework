75f95236b18e45b5da1aeef0ce3e5b6791e324ca
Anna Kijania <kijania.anna@gmail.com>
2020-06-04 15:08:33 +0200

    Fix function displayResult

--- js/script.js
+++ js/script.js
@@ -19,14 +19,6 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
-// if (randomNumber == 1) {
-//     computerMove = 'kamień';
-// } else if (randomNumber == 2) {
-//     computerMove = 'papier';
-// } else if (randomNumber == 3) {
-//     computerMove = 'nożyce';
-// }
-
 printMessage('Mój ruch to: ' + computerMove);
 
 // Player move
@@ -37,14 +29,6 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = getMoveName(playerInput);
 
-// if (playerInput == '1') {
-//     playerMove = 'kamień';
-// } else if (playerInput == '2') {
-//     playerMove = 'papier';
-// } else if (playerInput == '3') {
-//     playerMove = 'nożyce';
-// }
-
 printMessage('Twój ruch to: ' + playerMove);
 
 // Game score
@@ -54,7 +38,7 @@ function displayResult(argComputerMove, argPlayerMove) {
         printMessage('Ty wygrywasz!');
     } else if (argComputerMove == argPlayerMove) {
         printMessage('Jest remis!');
-    } else if ((argComputerMove == 'kamień' && pargPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'kamień')) {
+    } else if ((argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'kamień')) {
         printMessage('Ja wygrywam!');
     } else {
         printMessage('Nieznany ruch!');
